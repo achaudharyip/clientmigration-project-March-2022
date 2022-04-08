c26d07b6a0d3f95f676876f466693caa0fa55e71
AtulC <72448519+achaudharyip@users.noreply.github.com>
2022-04-08 11:05:36 -0400

    Add files to start the project

---
+++ Get-ADComputer-Details.ps1
@@ -0,0 +1,48 @@
+﻿<#
+.SYNOPSIS
+    Gather information of AD Computers
+.DESCRIPTION
+    Gather information of AD Computers to prepare and create a strategy for migrating the computers to a new domain
+.EXAMPLE
+    PS C:\> .\Get-ADComputer-Details.ps1
+    The script connects to the source domain, gathers information for computers, categorizes needed properties, and exports the properties to a csv file in a given order.
+.INPUTS
+    Inputs information for Domain and ComputerNames from YMCA-AD-DOMAIN-LIST.csv and YMCA-AD-HOST-LIST.csv
+.OUTPUTS
+    Output csv file with information for following:
+    HostName,FQDN,IPAddress,OS,OSVersion,ServiceAccount,OUPath,SID,GUID,GroupID
+.NOTES
+    Import from csv files with information for Domain Controller Server and HostNames
+#>
+
+function Get-ADCOMPUTERDETAILS {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$DOMAIN,
+        [string]$HOSTNAMES
+    ) #end param
+    process {
+        get-adcomputer -Server $DOMAIN -Identity $HOSTNAMES -Properties *
+    }
+}
+
+$DOMList = import-csv -Path C:\users\bsimigrate\Downloads\YMCA-AD-DOMAIN-LIST.csv
+$HOSTLIST = Import-Csv -Path C:\USERS\bsimigrate\Downloads\YMCA-AD-HOST-LIST.csv
+$DCSVR = $DOMList.SourceDomain
+$CLIENTS = $HOSTLIST.HostName
+$Details = @()
+foreach ($CLIENT in $CLIENTS) {
+    $Details += Get-ADCOMPUTERDETAILS $DCSVR $CLIENT
+}
+
+$Result = @()
+ForEach ($Detail in $Details){
+    If($Detail.ServiceAccount -eq '{}'){
+        $SVCACCT = "Null"
+        If($Detail.ServiceAccount -ne $null){
+            $SVCACCT = $Detail.ServiceAccount
+        }
+    }
+    $Result += New-Object -TypeName psobject -Property (@{FQDN=$Detail.DNSHostName;OUPath=$Detail.CanonicalName;HostName=$Detail.SamAccountName;IPAddress=$Detail.IPv4Address;SID=$Detail.SID;OS=$Detail.OperatingSystem;OSVersion=$Detail.OperatingSystemVersion;ServiceAccount=$SVCACCT;GUID=$Detail.ObjectGUID;GroupID=$Detail.primaryGroupID})
+}
+$Result|Select-Object -Property HostName,FQDN,IPAddress,OS,OSVersion,ServiceAccount,OUPath,SID,GUID,GroupID|Export-Csv -Path C:\Users\bsimigrate\Downloads\PreComputerMigrationInfo.csv -NoTypeInformation
---
+++ Get-ADUser-Details.ps1
@@ -0,0 +1,53 @@
+<#
+.SYNOPSIS
+    Gather information of AD Users
+.DESCRIPTION
+    Gather information of AD Users to prepare and create a strategy for migrating the Users to a new domain
+.EXAMPLE
+    PS C:\> .\Get-ADUsers-Details.ps1
+    The script connects to the source domain, gathers information for users, categorizes needed properties, and exports the properties to a csv file in a given order.
+.INPUTS
+    Inputs information for Domain and ComputerNames from YMCA-AD-DOMAIN-LIST.csv and YMCA-AD-USER-LIST.csv
+.OUTPUTS
+    Output csv file with information for following:
+    UserName,FirstName,LastName,AccountEnabled,GroupMembership,PrimaryGroup,PrimaryGrpID,HomeDrive,HomeDirectory,CompanyUPN,SID,UserOUPath,GUIDInfo
+.NOTES
+    Import from csv files with information for Domain Controller Server and HostNames
+#>
+
+function Get-ADUSERDETAIL {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$DOMAIN,
+        [string]$USRACCOUNTS
+    ) #end param
+    process {
+        get-aduser -Server $DOMAIN -Identity $USRACCOUNTS -Properties *
+    }
+}
+
+$DOMList = import-csv -Path "C:\users\bsimigrate\Downloads\YMCA-AD-DOMAIN-LIST.csv"
+$USERLIST = Import-Csv -Path C:\USERS\bsimigrate\Downloads\YMCA-AD-USER-LIST.csv
+$DCSVR = $DOMList.SourceDomain
+$CLIENTS = $USERLIST.UserName
+#$DCSVR = "dcv.stratford.local"
+#$USERS = "stratford.migrate","stratford.migrate2"
+$Details = @()
+foreach ($USER in $USERS) {
+    $Details += Get-ADUSERDETAIL $DCSVR $USER
+}
+
+$USRResult = @()
+ForEach ($Detail in $Details){
+    If($Detail.Enabled -eq $true){
+        $Enabled = "Yes"
+        If($Detail.Enabled -eq $false){
+            $Enabled = "No"
+        }
+    }
+    $Group = ((($Detail.MemberOf -split ",OU=O365 Groups,OU=Stratford Staff") -replace "CN=", "") -replace ",Builtin", "") -replace ",DC=Stratford,DC=local", ""| Out-String
+    $USRResult += New-Object -TypeName psobject -Property (@{PrimaryGroup=$Detail.PrimaryGroup;PrimaryGrpID=$Detail.primaryGroupID;HomeDrive=$Detail.HomeDrive;HomeDirectory=$Detail.HomeDirectory;Company=$Detail.Company;GroupMembership=$Group;UserName=$Detail.SamAccountName; AccountEnabled=$Enabled; FirstName=$Detail.GivenName; LastName=$Detail.Surname; UserOUPath=$Detail.DistinguishedName; SID=$Detail.SID; GUIDInfo=$Detail.ObjectGUID; UPN=$Detail.UserPrincipalName})
+}
+
+
+$USRResult|Select-Object -Property UserName,FirstName,LastName,AccountEnabled,GroupMembership,PrimaryGroup,PrimaryGrpID,HomeDrive,HomeDirectory,CompanyUPN,SID,UserOUPath,GUIDInfo|Export-Csv -Path C:\Users\bsimigrate\Downloads\PreUserMigrationInfo.csv -NoTypeInformation
