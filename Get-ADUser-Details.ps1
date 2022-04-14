ef789aa7def73d686731d82266256bc0b99d8f24
AtulC <72448519+achaudharyip@users.noreply.github.com>
2022-04-14 08:27:07 -0400

    Update Get-ADUSER-Details.ps1
    Added function to call Select-csvFile, changed report file name, and changed static path to user selected path

--- Get-ADUser-Details.ps1
+++ Get-ADUser-Details.ps1
@@ -7,7 +7,7 @@
     PS C:\> .\Get-ADUsers-Details.ps1
     The script connects to the source domain, gathers information for users, categorizes needed properties, and exports the properties to a csv file in a given order.
 .INPUTS
-    Inputs information for Domain and ComputerNames from COMPANY-AD-DOMAIN-LIST.csv and COMPANY-AD-USER-LIST.csv
+    Inputs information for Domain and ComputerNames from YMCA-AD-DOMAIN-LIST.csv and YMCA-AD-USER-LIST.csv
 .OUTPUTS
     Output csv file with information for following:
     UserName,FirstName,LastName,AccountEnabled,GroupMembership,PrimaryGroup,PrimaryGrpID,HomeDrive,HomeDirectory,CompanyUPN,SID,UserOUPath,GUIDInfo
@@ -15,6 +15,17 @@
     Import from csv files with information for Domain Controller Server and HostNames
 #>
 
+function Select-csvFile($startDir){
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")|Out-Null
+
+    $FileName = New-Object System.Windows.Forms.OpenFileDialog
+    $FileName.InitialDirectory = $startDir
+    $FileName.Filter = "All files (*.*)| *.*"
+    $FileName.ShowDialog() | Out-Null
+    $FileName.FileName
+}
+
+# 
 function Get-ADUSERDETAIL {
     param (
         [Parameter(Mandatory=$true)]
@@ -26,15 +37,25 @@ function Get-ADUSERDETAIL {
     }
 }
 
-$DOMList = import-csv -Path "C:\users\UserName\Downloads\COMPANY-AD-DOMAIN-LIST.csv"
-$USERLIST = Import-Csv -Path C:\USERS\UserName\Downloads\COMPANY-AD-USER-LIST.csv
-$DCSVR = $DOMList.SourceDomain
-$CLIENTS = $USERLIST.UserName
+$csvName = Select-csvFile
+$LISTS = import-csv -Path $csvName
+
 $Details = @()
-foreach ($USER in $USERS) {
+foreach ($LIST in $LISTS) {
+    $DCSVR = $List.Domain
+    $USER = $LIST.UserName
     $Details += Get-ADUSERDETAIL $DCSVR $USER
 }
 
+$Date = Get-Date -Format MMdyy
+$RptName = "PreUserMigrationReport-" + $Date + ".csv"
+$UserProfile = (Get-ChildItem -Path Env:\USERPROFILE).Value
+$DownloadsPath = $UserProfile + '\Downloads'
+$RptsPath = $DownloadsPath + '\' + 'PreMigrationReports'
+$RptFilePath = $RptsPath + "\" + $RptName
+If((Test-Path -Path $RptsPath) -ne $true){
+    New-Item -Path $DownloadsPath -ItemType Directory -Name 'PreMigrationReports'
+}
 $Result = @()
 ForEach ($Detail in $Details){
     If($Detail.Enabled -eq $true){
@@ -48,4 +69,4 @@ ForEach ($Detail in $Details){
 }
 
 
-$Result|Select-Object -Property UserName,FirstName,LastName,AccountEnabled,GroupMembership,PrimaryGroup,PrimaryGrpID,HomeDrive,HomeDirectory,CompanyUPN,SID,UserOUPath,GUIDInfo|Export-Csv -Path c:\Path-to-Directory-for-output-csv-file\PreUserMigrationInfo.csv -NoTypeInformation
+$Result|Select-Object -Property UserName,FirstName,LastName,AccountEnabled,GroupMembership,PrimaryGroup,PrimaryGrpID,HomeDrive,HomeDirectory,CompanyUPN,SID,UserOUPath,GUIDInfo|Export-Csv -Path $RptFilePath -Append -NoTypeInformation
