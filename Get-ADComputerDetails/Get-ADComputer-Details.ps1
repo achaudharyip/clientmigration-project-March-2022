3da367ae54d9c33a7b4a345508a7841e87dc0271
AtulC <72448519+achaudharyip@users.noreply.github.com>
2022-04-20 10:43:20 -0400

    Add files

---
+++ Get-ADComputerDetails/Get-ADComputer-Details.ps1
@@ -0,0 +1,69 @@
+﻿<#
+.SYNOPSIS
+    Gather information of AD Computers
+.DESCRIPTION
+    Gather information of AD Computers to prepare and create a strategy for migrating the computers to a new domain
+.EXAMPLE
+    PS C:\> .\Get-ADComputer-Details.ps1
+    The script connects to the source domain, gathers information for computers, categorizes needed properties, and exports the properties to a csv file in a given order.
+.INPUTS
+    Inputs information for Domain and ComputerNames from YMCA-AD-DOMAIN-LIST.csv and YMCA-AD-HOST-LIST.csv
+.OUTPUTS
+    Output csv file with information for following:
+    HostName,FQDN,IPAddress,OS,OSVersion,ServiceAccount,OUPath,SID,GUID,GroupID
+.NOTES
+    Import from csv files with information for Domain Controller Server and HostNames
+#>
+
+function Select-csvFile($startDir){
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")|Out-Null
+
+    $FileName = New-Object System.Windows.Forms.OpenFileDialog
+    $FileName.InitialDirectory = $startDir
+    $FileName.Filter = "All files (*.*)| *.*"
+    $FileName.ShowDialog() | Out-Null
+    $FileName.FileName
+}
+
+function Get-ADCOMPUTERDETAILS {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$DOMAIN,
+        [string]$HOSTNAMES
+    ) #end param
+    process {
+        get-adcomputer -Server $DOMAIN -Identity $HOSTNAMES -Properties *
+    }
+}
+
+$csvName = Select-csvFile
+$LISTS = import-csv -Path $csvName
+
+$Details = @()
+foreach ($LIST in $LISTS) {
+    $DCSVR = $LIST.Domain
+    $CLIENT = $LIST.HostName
+    $Details += Get-ADCOMPUTERDETAILS $DCSVR $CLIENT
+}
+
+$Date = Get-Date -Format MMddyy
+$RptName = "PreHostMigrationReport-" + $Date + ".csv"
+$UserProfile = (Get-ChildItem -Path Env:\USERPROFILE).Value
+$DownloadsPath = $UserProfile + '\Downloads'
+$RptsPath = $DownloadsPath + '\' + 'PreMigrationReports'
+$RptFilePath = $RptsPath + "\" + $RptName
+If((Test-Path -Path $RptsPath) -ne $true){
+    New-Item -Path $DownloadscPath -ItemType Directory -Name 'PreMigrationReports'
+}
+
+$Result = @()
+ForEach ($Detail in $Details){
+    If($Detail.ServiceAccount -eq '{}'){
+        $SVCACCT = "Null"
+        If($Detail.ServiceAccount -ne $null){
+            $SVCACCT = $Detail.ServiceAccount
+        }
+    }
+    $Result += New-Object -TypeName psobject -Property (@{FQDN=$Detail.DNSHostName;OUPath=$Detail.CanonicalName;HostName=$Detail.SamAccountName;IPAddress=$Detail.IPv4Address;SID=$Detail.SID;OS=$Detail.OperatingSystem;OSVersion=$Detail.OperatingSystemVersion;ServiceAccount=$SVCACCT;GUID=$Detail.ObjectGUID;GroupID=$Detail.primaryGroupID})
+}
+$Result|Select-Object -Property HostName,FQDN,IPAddress,OS,OSVersion,ServiceAccount,OUPath,SID,GUID,GroupID|Export-Csv -Path $RptFilePath -Append -NoTypeInformation
---
+++ Grant-LocalAdminAccess/Grant-LocalAdminAccess.ps1
@@ -0,0 +1,98 @@
+<#
+.SYNOPSIS
+    Grant Local Admin Access
+.DESCRIPTION
+    Grant User performing migration to multiple computers remotely
+.EXAMPLE
+    PS C:\> .\Grant-LocalAdminAccess.ps1
+    Connects to remote machine and adds inputed user account to local admin group of remote computers
+.INPUTS
+    Inputs information for Hosts and users from one csv file. Get Credential for Domain Admin or permissions to change access on computers in source domain.
+    UserAccounts must be in the format: Domain\UserName
+.OUTPUTS
+    Output csv file report Account is added on remote computer as success or failed
+.NOTES
+    Import from csv files with information for Domain Controller Server, UserNames, and HostNames
+#>
+
+Clear-Host
+Add-Type -AssemblyName PresentationFramework
+function Select-csvFile($startDir){
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")|Out-Null
+
+    $FileName = New-Object System.Windows.Forms.OpenFileDialog
+    $FileName.InitialDirectory = $startDir
+    $FileName.Filter = "All files (*.*)| *.*"
+    $FileName.ShowDialog() | Out-Null
+    $FileName.FileName
+}
+
+function Set-Path($startdir="") {
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+
+    $Directory = New-Object System.Windows.Forms.FolderBrowserDialog
+    $Directory.Description = "Select the path to the psexec and grant-localadminaccess.psq directory"
+    $Directory.rootfolder = "MyComputer"
+    $Directory.SelectedPath = $startdir
+
+    if($Directory.ShowDialog() -eq "OK")
+    {
+        $adminacesspath += $Directory.SelectedPath
+    }
+    return $Directory
+}
+
+$path = (Set-Path).SelectedPath
+$cred = Get-Credential
+$psexec = Select-csvFile       
+$csvFile = Select-csvFile
+$LISTS = Import-Csv $csvFile -Header HostName, UserName
+$HOSTS = $LISTS.HostName
+$USER = $cred.UserName
+$P = $Cred.GetNetworkCredential().Password
+Clear-Host
+ForEach($CNAME in $HOSTS){
+    If((Test-Connection -ComputerName $CNAME -Quiet) -eq $true){
+        Set-Location -Path $path
+        $Remote = "\\" + $CNAME
+        $ADMPath = $Remote + '\admin$'
+        Write-Host "Starting temporary net connection to $ADMPath" -ForegroundColor Green
+        net use $ADMPath /user:$USER $P
+        Start-Process -Wait -PSPath $PSexec -ArgumentList "-u $cred.UserName -p $cred.password $Remote -h -s winrm.cmd quickconfig -q" -Verb RunAs
+        Write-Host "Enabling WinRM on $CNAME" -ForegroundColor Green
+        Start-Sleep -Seconds 60 -Verbose
+        Write-Host "Waiting until WinRM on $CNAME is complete" -ForegroundColor Yellow
+        Start-Sleep -Seconds 60 -Verbose
+        Start-Process -Wait -PSPath $PSexec -ArgumentList "-u $cred.UserName -p $cred.Password $Remote -c PowerShell.exe enable-psremoting -force" -Verb RunAs
+        Write-Host "Enabling PSRemoting on $CNAME" -ForegroundColor Green
+        Start-Process -Wait -PSPath $PSexec -ArgumentList "-u $cred.UserName -p $cred.Password $Remote -c PowerShell.exe set-executionpolicy RemoteSigned -force" -Verb RunAs
+        Write-Host "Enabling and setting the execution policy on $CNAME to RemoteSigned" -ForegroundColor Green
+        Write-Host "Deleting temporary net connection to $ADMPath" -ForegroundColor Green
+        net use $ADMPath /DELETE
+        IF((Test-WSMan -ComputerName $CNAME) -ne $false){
+            Write-Host "PSRemoting on $CNAME is now working" -ForegroundColor Green
+            }
+        }
+    }
+
+$Status = @()
+ForEach ($LIST in $LISTS){
+    If((Test-Connection -ComputerName $LIST.HostName -Quiet) -eq $true){
+        $HOSTNAME = $LIST.HostName
+        $USERNAME = $LIST.UserName
+        If((Test-WSMan -ComputerName $HOSTNAME) -ne $false){
+            Write-Host "Starting a new remote session to $HOSTNAME" -ForegroundColor Green
+            $S = New-PSSession -ComputerName $HOSTNAME -Credential $CRED
+            Invoke-Command -Session $S -ScriptBlock { Add-LocalGroupMember -Group "Administrators" -Member $USING:UserName }
+            $CheckSuccess = Invoke-Command -Session $S -ScriptBlock { Get-LocalGroupMember -Group "Administrators" -Member $args[0] } -ArgumentList $USERNAME
+            If($CheckSuccess.Name -eq $USERNAME){
+                $ACCTSTATUS = "True"
+            }
+            Else {
+                $ACCTSTATUS = "False"
+            }
+            Remove-PSSession $S
+            $Status += New-Object -TypeName psobject -Property (@{HostName=$HOSTNAME;Status=$ACCTSTATUS;UserName=$USER;Group=$GROUP})
+            }
+    }
+}
