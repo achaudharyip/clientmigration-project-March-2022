d776d55c160fd42110c3b722e9efbaadec94e499
AtulC <72448519+achaudharyip@users.noreply.github.com>
2022-05-21 10:01:44 -0400

    Update Grant-LocalAdminAccess.ps1
    add report path to save and fix bug which caused the script to not work if the client is unreachable

--- Grant-LocalAdminAccess/Grant-LocalAdminAccess.ps1
+++ Grant-LocalAdminAccess/Grant-LocalAdminAccess.ps1
@@ -31,7 +31,7 @@ function Set-Path($startdir="") {
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
 
     $Directory = New-Object System.Windows.Forms.FolderBrowserDialog
-    $Directory.Description = "Select the path to the psexec and grant-localadminaccess.ps1 directory"
+    $Directory.Description = "Select the path to the grant-localadminaccess.ps1 directory"
     $Directory.rootfolder = "MyComputer"
     $Directory.SelectedPath = $startdir
 
@@ -41,32 +41,50 @@ function Set-Path($startdir="") {
     }
     return $Directory
 }
-
+Write-Host "Select the path where the ps1 file is located..." -ForegroundColor Green
 $path = (Set-Path).SelectedPath
+Write-Host "Enter the Domain Admin Credentials for the domain..." -ForegroundColor Green
 $cred = Get-Credential
-$psexec = (Get-ChildItem -Path $path "psexec.exe").Name       
+Write-Host "Settig full path for psexec.exe..." -ForegroundColor Green
+$psexec = (Get-ChildItem -Path $path "psexec.exe").Name
+Write-Host "Select the csv file with HostName and credentials to be added to the local Administrators Group..." -ForegroundColor Green  
 $csvFile = Select-csvFile
+Write-Host "Importing the csv File..." -ForegroundColor Green
 $LISTS = Import-Csv $csvFile -Header HostName, UserName
 $HOSTS = $LISTS.HostName
 $USER = $cred.UserName
 $P = $Cred.GetNetworkCredential().Password
+Write-Host "Setting Reports Path Name and Directory Path..." -ForegroundColor Green
+$Date = Get-Date -Format MMddyy
+$RptName = "AddAAccountAdminGrp_" + $Date + ".csv"
+$UserProfile = (Get-ChildItem -Path Env:\USERPROFILE).Value
+$DownloadsPath = $UserProfile + '\Downloads'
+$RptsPath = $DownloadsPath + '\' + 'PreMigrationReports'
+$RptFilePath = $RptsPath + "\" + $RptName
+If((Test-Path -Path $RptsPath) -ne $true){
+    New-Item -Path $DownloadscPath -ItemType Directory -Name 'PreMigrationReports'
+}
 Clear-Host
 ForEach($CNAME in $HOSTS){
-    If((Test-Connection -ComputerName $CNAME -Quiet) -eq $true){
+    If((Test-Connection -ComputerName $CNAME -Quiet) -eq $false){
+        Write-Host "Set path to $path..." -ForegroundColor Green
         Set-Location -Path $path
         $Remote = "\\" + $CNAME
         $ADMPath = $Remote + '\admin$'
         Write-Host "Starting temporary net connection to $ADMPath" -ForegroundColor Green
         net use $ADMPath /user:$USER $P
+        Write-Host "Enabling winrm on $CNAME..." -ForegroundColor Green
         Start-Process -Wait -PSPath $PSexec -ArgumentList "-u $cred.UserName -p $cred.password $Remote -h -s winrm.cmd quickconfig -q" -Verb RunAs
-        Write-Host "Enabling WinRM on $CNAME" -ForegroundColor Green
+        Write-Host "Enabled WinRM on $CNAME..." -ForegroundColor Green
         Start-Sleep -Seconds 60 -Verbose
         Write-Host "Waiting until WinRM on $CNAME is complete" -ForegroundColor Yellow
         Start-Sleep -Seconds 60 -Verbose
+        Write-Host "Enabling PSRemoting on $CNAME..." -ForegroundColor Green
         Start-Process -Wait -PSPath $PSexec -ArgumentList "-u $cred.UserName -p $cred.Password $Remote -c PowerShell.exe enable-psremoting -force" -Verb RunAs
-        Write-Host "Enabling PSRemoting on $CNAME" -ForegroundColor Green
+        Write-Host "Enabled PSRemoting on $CNAME" -ForegroundColor Green
+        Write-Host "Enabling and setting the execution policy on $CNAME to RemoteSigned..." -ForegroundColor Green
         Start-Process -Wait -PSPath $PSexec -ArgumentList "-u $cred.UserName -p $cred.Password $Remote -c PowerShell.exe set-executionpolicy RemoteSigned -force" -Verb RunAs
-        Write-Host "Enabling and setting the execution policy on $CNAME to RemoteSigned" -ForegroundColor Green
+        Write-Host "Enabled/ Set the execution policy on $CNAME to RemoteSigned" -ForegroundColor Green
         Write-Host "Deleting temporary net connection to $ADMPath" -ForegroundColor Green
         net use $ADMPath /DELETE
         IF((Test-WSMan -ComputerName $CNAME) -ne $false){
@@ -83,6 +101,7 @@ ForEach ($LIST in $LISTS){
         If((Test-WSMan -ComputerName $HOSTNAME) -ne $false){
             Write-Host "Starting a new remote session to $HOSTNAME" -ForegroundColor Green
             $S = New-PSSession -ComputerName $HOSTNAME -Credential $CRED
+            Write-Host "Adding $USERNAME on $HOSTNAME to the local Administrators Group..." -ForegroundColor Green
             Invoke-Command -Session $S -ScriptBlock { Add-LocalGroupMember -Group "Administrators" -Member $USING:UserName }
             $CheckSuccess = Invoke-Command -Session $S -ScriptBlock { Get-LocalGroupMember -Group "Administrators" -Member $args[0] } -ArgumentList $USERNAME
             If($CheckSuccess.Name -eq $USERNAME){
@@ -96,3 +115,5 @@ ForEach ($LIST in $LISTS){
             }
     }
 }
+Write-Host "Writing the status results to file..." -ForegroundColor Green
+$Status|Select-Object -Property HostName,Status,UserName,Group|Export-Csv -Path $RptFilePath -Append -NoTypeInformation
